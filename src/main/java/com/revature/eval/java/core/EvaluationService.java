c4a686df2c1d1628471ee1050a70731410145d67
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-05-29 15:57:19 -0500

    Added and tested numberInWord. 54 failures left

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -207,8 +207,19 @@ public class EvaluationService {
 	 * statement or switch statement whatever is easier for you.
 	 */
 	public String printNumberInWord(int number) {
-		// TODO Write an implementation for this method declaration
-		return null;
+		switch(number) { // Determines case based on input, returns appropriate string
+			case 0: return "ZERO"; 
+			case 1: return "ONE";
+			case 2: return "TWO"; 
+			case 3: return "THREE"; 
+			case 4: return "FOUR"; 
+			case 5: return "FIVE"; 
+			case 6: return "SIX";
+			case 7: return "SEVEN";
+			case 8: return "EIGHT";
+			case 9: return "NINE"; 
+			default: return "OTHER";
+		}
 	}
 
 	/**
